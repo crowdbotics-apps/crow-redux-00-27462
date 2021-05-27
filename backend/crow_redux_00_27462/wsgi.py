d00c8b47dd27cbfbd071c5369bf647ecaa374770
Crowdbotics <team@crowdbotics.com>
2021-05-27 05:34:07 +0000

    Initial commit.

---
+++ backend/crow_redux_00_27462/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for crow_redux_00_27462 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crow_redux_00_27462.settings')
+
+application = get_wsgi_application()
